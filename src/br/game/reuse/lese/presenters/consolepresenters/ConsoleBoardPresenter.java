dafff97f884c45096cb7a610aa10fffdc68ad2a3
Marlon Marques <marlonbr1@gmail.com>
2016-05-01 15:33:08 -0300

    moving concrete presenters

---
+++ src/br/game/reuse/lese/presenters/consolepresenters/ConsoleBoardPresenter.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.game.reuse.lese.presenters.consolepresenters;
+
+import br.game.reuse.lese.board.Player;
+import br.game.reuse.lese.presenters.interfaces.BoardPresenter;
+
+/**
+ *
+ * @author marlon
+ */
+public class ConsoleBoardPresenter implements BoardPresenter {
+
+    @Override
+    public void showStartScreen() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void showSetUpScreen() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getNumPlayers() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void showWinner(Player player) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/br/game/reuse/lese/presenters/consolepresenters/ConsoleProjectPresenter.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.game.reuse.lese.presenters.consolepresenters;
+
+import br.game.reuse.lese.presenters.interfaces.ProjectPresenter;
+
+/**
+ *
+ * @author marlon
+ */
+public class ConsoleProjectPresenter implements ProjectPresenter {
+
+    @Override
+    public void showProjects() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getPlayerChoice() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
