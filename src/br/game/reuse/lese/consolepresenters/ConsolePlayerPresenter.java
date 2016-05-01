65c490eb101c1ae495d5908df36ae748481ced6e
Marlon Marques <marlonbr1@gmail.com>
2016-05-01 15:10:44 -0300

    fix package name

---
+++ src/br/game/reuse/lese/consolepresenters/ConsolePlayerPresenter.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.game.reuse.lese.consolepresenters;
+
+import br.game.reuse.lese.presenters.interfaces.PlayerPresenter;
+
+/**
+ *
+ * @author marlon
+ */
+public class ConsolePlayerPresenter implements PlayerPresenter {
+
+    @Override
+    public void showPlayerSetUp() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getPlayerName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void showMovement() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/br/game/reuse/lese/consolepresenters/ConsoleQuestionPresenter.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.game.reuse.lese.consolepresenters;
+
+import br.game.reuse.lese.presenters.interfaces.QuestionPresenter;
+
+/**
+ *
+ * @author marlon
+ */
+public class ConsoleQuestionPresenter implements QuestionPresenter {
+
+    @Override
+    public void showQuestion() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void showChoices() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getPlayerAnswer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void showFeedback() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
