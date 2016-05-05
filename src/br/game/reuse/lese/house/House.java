7ff19dff545b3441707ba38f65d29e2f89a644bf
bruno <bruno@linux-PC>
2016-05-05 18:51:41 -0300

    Removed method showContent. It wasn't necessary.

--- src/br/game/reuse/lese/house/House.java
+++ src/br/game/reuse/lese/house/House.java
@@ -39,8 +39,6 @@ public abstract class House {
     public DevelopmentPhase getDevPhase() {
         return this.devPhase;
     }
-    
-    public abstract void showContent(Player p);
         
     //Design Pattern: Template Method
     public final void execute(Player p) {
