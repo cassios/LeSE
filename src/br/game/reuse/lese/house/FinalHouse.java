5823463e0a4e01438c957c59a99c493b15b20b4d
bruno <bruno@linux-PC>
2016-05-11 16:59:53 -0300

    Layer view code moved to presenter classes.

--- src/br/game/reuse/lese/house/FinalHouse.java
+++ src/br/game/reuse/lese/house/FinalHouse.java
@@ -8,6 +8,10 @@ package br.game.reuse.lese.house;
 import br.game.reuse.lese.board.DevelopmentPhase;
 import br.game.reuse.lese.board.PlayerBoard;
 import br.game.reuse.lese.outcome.HouseOutcome;
+import br.game.reuse.lese.presenters.consolepresenters.ConsoleFinalHousePresenter;
+import br.game.reuse.lese.presenters.consolepresenters.ConsoleHousePresenter;
+import br.game.reuse.lese.presenters.interfaces.HousePresenter;
+import br.game.reuse.lese.presenters.interfaces.MessagePresenter;
 
 /**
  *
@@ -27,21 +31,21 @@ public class FinalHouse extends House {
 
     @Override
     protected void presentContent() {
-        System.out.println(this.message);
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//        System.out.println(this.message);
     }
 
     @Override
     protected void interactWithPlayer(PlayerBoard p) {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        MessagePresenter finalPresenter = new ConsoleFinalHousePresenter();
+        finalPresenter.showContent(this);
+        finalPresenter.continueGame();
     }
 
     @Override
     protected void applyOutcome(PlayerBoard p) {
         HouseOutcome outcome = getOutcome();
         outcome.apply(p, this);
-        System.out.println("Casa Atual: " + (p.getPawnPosition().getId() + 1));
-        System.out.println("Pontuação Atual: " + p.getCurrentScore());
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        HousePresenter housePresenter = new ConsoleHousePresenter();
+        housePresenter.showHouseInfo(p);
     }
 }
