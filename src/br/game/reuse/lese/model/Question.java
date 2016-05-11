7e6aeec22b42c9ee28943015d9a7b21e8b248f2e
bruno <bruno@linux-PC>
2016-05-11 02:42:59 -0300

    Class refactored.

--- src/br/game/reuse/lese/model/Question.java
+++ src/br/game/reuse/lese/model/Question.java
@@ -11,83 +11,56 @@ package br.game.reuse.lese.model;
  */
 public class Question {
     
-    private int code = 0;
-    private Phase phase = null;
-    private Projects project = null;
-    private int cycle = 0;
-    private String text = "";
-    private String explication = "";
+    private int idQuestion = 0;
+    private Phase phase = new Phase();
+    private String description = "";
+    private String explanation = "";
     private int score = 0;
+    private int house = 0;
 
     public Question() {
         //constructor empty.
     }
 
-    public Question(Phase newPhase, Projects newProject, int newCycle, String newText, String newExplication, int newScore) {
+    public Question(Phase newPhase, String newDescription, String newExplanation, int newScore, int newHouse) {
         this.phase = newPhase;
-        this.project = newProject;
-        this.cycle = newCycle;
-        this.text = newText;
-        this.explication = newExplication;
+        this.description = newDescription;
+        this.explanation = newExplanation;
         this.score = newScore;
+        this.house = newHouse;
     }
 
-    public Question(int newCode, Phase newPhase, Projects newProject, int newCycle, String newText, String newExplication, int newScore) {
-        this.code = newCode;
-        this.project = newProject;
-        this.cycle = newCycle;
+    public Question(int newIdQuestion, Phase newPhase, String newDescription, String newExplanation, int newScore, int newHouse) {
+        this.idQuestion = newIdQuestion;
         this.phase = newPhase;
-        this.text = newText;
-        this.explication = newExplication;
+        this.description = newDescription;
+        this.explanation = newExplanation;
         this.score = newScore;
+        this.house = newHouse;
     }
 
     public int getCode() {
-        return code;
+        return idQuestion;
     }
 
-    public void setCode(int newCode) {
-        this.code = newCode;
+    public void setCode(int newIdQuestion) {
+        this.idQuestion = newIdQuestion;
     }
 
-    public Phase getPhase() {
-        return phase;
+    public String getDescription() {
+        return description;
     }
 
-    public void setPhase(Phase newPhase) {
-        this.phase = newPhase;
+    public void setDescription(String newDescription) {
+        this.description = newDescription;
     }
 
-    public Projects getProject() {
-        return project;
+    public String getExplanation() {
+        return explanation;
     }
 
-    public void setProject(Projects newProject) {
-        this.project = newProject;
-    }
-
-    public int getCycle() {
-        return cycle;
-    }
-
-    public void setCycle(int newCycle) {
-        this.cycle = newCycle;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String newText) {
-        this.text = newText;
-    }
-
-    public String getExplication() {
-        return explication;
-    }
-
-    public void setExplication(String newExplication) {
-        this.explication = newExplication;
+    public void setExplanation(String newExplanation) {
+        this.explanation = newExplanation;
     }
 
     public int getScore() {
@@ -96,6 +69,14 @@ public class Question {
 
     public void setScore(int newScore) {
         this.score = newScore;
+    }
+
+    public int getHouse() {
+        return house;
+    }
+
+    public void setHouse(int newHouse) {
+        this.house = newHouse;
     }
     
 }
