128adfca05d7c41d15b3a9aa178e4f4b9793444a
bruno <bruno@linux-PC>
2016-05-11 17:01:38 -0300

    Interface created.

---
+++ src/br/game/reuse/lese/presenters/interfaces/MessagePresenter.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.game.reuse.lese.presenters.interfaces;
+
+import br.game.reuse.lese.house.House;
+
+/**
+ *
+ * @author bruno
+ */
+public interface MessagePresenter {
+
+    /**
+     * Displays the question to the player.
+     *
+     * @param house
+     */
+    void showContent(House house);
+
+    /**
+     * Displays question choices to the player.
+     *
+     */
+    void continueGame();
+
+}
