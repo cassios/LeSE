89bbcf77d2383d85c5082eb19f6bbed241c580f9
bruno <bruno@linux-PC>
2016-05-11 17:01:23 -0300

    Interface refactored.

--- src/br/game/reuse/lese/presenters/interfaces/QuestionPresenter.java
+++ src/br/game/reuse/lese/presenters/interfaces/QuestionPresenter.java
@@ -5,6 +5,8 @@
  */
 package br.game.reuse.lese.presenters.interfaces;
 
+import br.game.reuse.lese.question.QuestionBoard;
+
 /**
  *
  * @author marlon
@@ -14,22 +16,26 @@ public interface QuestionPresenter {
     /**
      * Displays the question to the player.
      *
+     * @param question
      */
-    void showQuestion();
+    void showQuestion(QuestionBoard question);
     
     /**
      * Displays question choices to the player.
+     * @param question
      */
-    void showChoices();
+    void showChoices(QuestionBoard question);
     
     /**
      * Obtains answer from the player
+     * @param question
      * @return player answer
      */
-    String getPlayerAnswer();
+    String getPlayerAnswer(QuestionBoard question);
     
     /**
      * Gives feedback to the user based on their answer.
+     * @param feedback
      */
-    void showFeedback();
+    void showFeedback(String feedback);
 }
