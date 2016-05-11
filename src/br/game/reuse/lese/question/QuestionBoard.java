0ae71c98811d2006ca8eedf0c0bd8124640bbfe9
bruno <bruno@linux-PC>
2016-05-11 03:02:15 -0300

    Class renamed.

---
+++ src/br/game/reuse/lese/question/QuestionBoard.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.game.reuse.lese.question;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author cass
+ */
+public class QuestionBoard {
+    private String description;
+    private String explanation;
+    private Set<String> choices;
+    private String answer;
+    
+    public QuestionBoard(String description, String explanation) {
+        this.description = description;
+        this.explanation = explanation;
+        this.choices = new HashSet<>();
+    }
+    
+    public String getDescription() {
+        return this.description;
+    }
+    
+    public String getEplanation() {
+        return this.explanation;
+    }
+    
+    public void addChoice(String choice, boolean isAnswer) {
+        this.choices.add(choice);
+        if(isAnswer) this.answer = choice;
+    }
+        
+    public Set<String> getChoices() {
+        return this.choices;
+    }
+    
+    public boolean verifyAnswer(String playerAnswer) {
+        return playerAnswer.equals(this.answer);
+    }
+}
