b808093a929daf379168bf84c2fc74bc98ab9594
bruno <bruno@linux-PC>
2016-05-12 13:20:52 -0300

    Method presentContent in the classes JokerHouse ande QuestionHouse implemented to update the questions and jokers.

--- src/br/game/reuse/lese/house/JokerHouse.java
+++ src/br/game/reuse/lese/house/JokerHouse.java
@@ -7,6 +7,12 @@ package br.game.reuse.lese.house;
 
 import br.game.reuse.lese.board.DevelopmentPhase;
 import br.game.reuse.lese.board.PlayerBoard;
+import br.game.reuse.lese.content.ContentConstruction;
+import br.game.reuse.lese.content.ContentElaboration;
+import br.game.reuse.lese.content.ContentInception;
+import br.game.reuse.lese.content.ContentTemplate;
+import br.game.reuse.lese.content.ContentTransition;
+import br.game.reuse.lese.content.ContentVerification;
 import br.game.reuse.lese.outcome.HouseOutcome;
 import br.game.reuse.lese.presenters.consolepresenters.ConsoleHousePresenter;
 import br.game.reuse.lese.presenters.consolepresenters.ConsoleJokerHousePresenter;
@@ -36,8 +42,26 @@ public class JokerHouse extends House {
 
     @Override
     protected void presentContent() {
-        
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ContentTemplate contentTemplate = null;
+        switch(getDevPhase()){
+            case Inception:
+                contentTemplate = ContentInception.getInstance();
+                break;
+            case Elaboration:
+                contentTemplate = ContentElaboration.getInstance();
+                break;
+            case Construction:
+                contentTemplate = ContentConstruction.getInstance();
+                break;
+            case Verification:
+                contentTemplate = ContentVerification.getInstance();
+                break;
+            case Transition:
+                contentTemplate = ContentTransition.getInstance();
+                break;
+        }
+        House houseUpdated = contentTemplate.refreshJoker(this);
+        updateContent(houseUpdated);
     }
 
     @Override
@@ -53,5 +77,11 @@ public class JokerHouse extends House {
         outcome.apply(p, this);
         HousePresenter housePresenter = new ConsoleHousePresenter();
         housePresenter.showHouseInfo(p);
+    }
+    
+    public void updateContent(House house){
+        JokerHouse j = (JokerHouse)house;
+        setOutcome(j.getOutcome());
+        setJoker(j.getJoker());
     }
 }
--- src/br/game/reuse/lese/house/QuestionHouse.java
+++ src/br/game/reuse/lese/house/QuestionHouse.java
@@ -5,14 +5,22 @@
  */
 package br.game.reuse.lese.house;
 
+import br.game.reuse.lese.board.Board;
 import br.game.reuse.lese.board.DevelopmentPhase;
 import br.game.reuse.lese.board.PlayerBoard;
+import br.game.reuse.lese.content.ContentConstruction;
+import br.game.reuse.lese.content.ContentElaboration;
+import br.game.reuse.lese.content.ContentInception;
+import br.game.reuse.lese.content.ContentTemplate;
+import br.game.reuse.lese.content.ContentTransition;
+import br.game.reuse.lese.content.ContentVerification;
 import br.game.reuse.lese.outcome.HouseOutcome;
 import br.game.reuse.lese.presenters.consolepresenters.ConsoleHousePresenter;
 import br.game.reuse.lese.presenters.consolepresenters.ConsoleQuestionPresenter;
 import br.game.reuse.lese.presenters.interfaces.HousePresenter;
 import br.game.reuse.lese.presenters.interfaces.QuestionPresenter;
 import br.game.reuse.lese.question.QuestionBoard;
+import java.util.List;
 
 /**
  *
@@ -39,15 +47,34 @@ public class QuestionHouse extends House {
 
     @Override
     protected void presentContent() {
-
+        ContentTemplate contentTemplate = null;
+        switch(getDevPhase()){
+            case Inception:
+                contentTemplate = ContentInception.getInstance();
+                break;
+            case Elaboration:
+                contentTemplate = ContentElaboration.getInstance();
+                break;
+            case Construction:
+                contentTemplate = ContentConstruction.getInstance();
+                break;
+            case Verification:
+                contentTemplate = ContentVerification.getInstance();
+                break;
+            case Transition:
+                contentTemplate = ContentTransition.getInstance();
+                break;
+        }
+        House houseUpdated = contentTemplate.refreshQuestion(this);
+        updateContent(houseUpdated);
     }
 
     @Override
     protected void interactWithPlayer(PlayerBoard p) {
         QuestionPresenter questionPresenter = new ConsoleQuestionPresenter();
         questionPresenter.showQuestion(this.question);
-        questionPresenter.showChoices(this.question);
-        String answer = questionPresenter.getPlayerAnswer(this.question);
+        List<String> choices = questionPresenter.showChoices(this.question);
+        String answer = questionPresenter.getPlayerAnswer(choices);
         this.correct = this.question.verifyAnswer(answer);
     }
 
@@ -62,5 +89,11 @@ public class QuestionHouse extends House {
 
     public boolean isCorrect() {
         return this.correct;
+    }
+    
+    public void updateContent(House house){
+        QuestionHouse q = (QuestionHouse)house;
+        setOutcome(q.getOutcome());
+        setQuestion(q.getQuestion());
     }
 }
