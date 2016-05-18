1d5c5a6c5a6c13e55231d99a38d5a5105c46c929
bruno <bruno@linux-PC>
2016-05-18 14:58:21 -0300

    Added switch to execute menu's options.

--- src/br/game/reuse/lese/board/BoardGame.java
+++ src/br/game/reuse/lese/board/BoardGame.java
@@ -7,8 +7,10 @@ package br.game.reuse.lese.board;
 
 import br.game.reuse.lese.house.House;
 import br.game.reuse.lese.presenters.consolepresenters.ConsoleBoardPresenter;
+import br.game.reuse.lese.presenters.consolepresenters.ConsoleGamePresenter;
 import br.game.reuse.lese.presenters.consolepresenters.ConsolePlayerPresenter;
 import br.game.reuse.lese.presenters.interfaces.BoardPresenter;
+import br.game.reuse.lese.presenters.interfaces.GamePresenter;
 import br.game.reuse.lese.presenters.interfaces.PlayerPresenter;
 
 /**
@@ -16,50 +18,70 @@ import br.game.reuse.lese.presenters.interfaces.PlayerPresenter;
  * @author cass
  */
 public class BoardGame {
+
     private Board board;
-    
-    
+
     public void creatBoard(BoardBuilder builder) {
-        
+
         builder.buildBoard();
         builder.buildProject();
         builder.buildHouses();
     }
-    
+
     public void setUpGame() {
         PlayerPresenter playerPresenter = new ConsolePlayerPresenter();
-        int nPlayers = playerPresenter.showPlayerSetUp();        
-        for (int i = 1; i <= nPlayers; i++) {
-            String playerName = playerPresenter.getPlayerName(i);
-            this.board.addPlayer(playerName, "red");
-        }
+        String playerName = playerPresenter.getPlayerName();
+        this.board.addPlayer(playerName, "red");
     }
-    
-    public void init(){
+
+    public void init() {
         creatBoard(new BoardBuilder());
         this.board = Board.getInstance();
-        setUpGame();
-        run();
+        GamePresenter gamePresenter = new ConsoleGamePresenter();
+        gamePresenter.welcome();
+        int opcao = 0;
+        do {
+            opcao = gamePresenter.menu();
+            switch (opcao) {
+                case 1:
+                    setUpGame();
+                    run();
+                    break;
+                case 2:
+                    gamePresenter.showRanking();
+                    break;
+                case 3:
+                    gamePresenter.showHelp();
+                    break;
+                case 4:
+                    gamePresenter.showExit();
+                    break;
+                default:
+                    gamePresenter.showDefault();
+                    break;
+            }
+        } while (opcao != 4);
     }
-    
+
     public void run() {
         PlayerBoard winner = null;
-        
-        while(winner == null) {
+
+        while (winner == null) {
             for (PlayerBoard p : this.board.getPlayers()) {
-               House playerHouse = this.board.getPlayerHouse(p);
-               playerHouse.execute(p);
-               
-               if(this.board.hasWinner()) {
-                   winner = this.board.getWinner();
-                   break;
-               }
+                House playerHouse = this.board.getPlayerHouse(p);
+                playerHouse.execute(p);
+
+                if (this.board.hasWinner()) {
+                    winner = this.board.getWinner();
+                    this.board.storePlayers();
+                    break;
+                }
             }
         }
-        
-        if(winner != null) {
+
+        if (winner != null) {
             BoardPresenter boarPresenter = new ConsoleBoardPresenter();
             boarPresenter.showWinner(winner);
         }
-    }    
+    }
 }
