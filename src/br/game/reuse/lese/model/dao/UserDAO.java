517919ca427301f5b002147e216f785ba6ff082c
bruno <bruno@linux-PC>
2016-05-18 15:02:56 -0300

    Methods created to classes DAO.

--- src/br/game/reuse/lese/model/dao/UserDAO.java
+++ src/br/game/reuse/lese/model/dao/UserDAO.java
@@ -5,10 +5,12 @@
  */
 package br.game.reuse.lese.model.dao;
 
+import br.game.reuse.lese.board.PlayerBoard;
 import br.game.reuse.lese.connection.DBConnection;
 import br.game.reuse.lese.model.User;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,8 +20,8 @@ import java.util.logging.Logger;
  * @author bruno
  */
 public class UserDAO {
-    
-    public static int createUser(User user){
+
+    public static int createUser(User user) {
         Connection connection = null;
         PreparedStatement ps = null;
         try {
@@ -31,10 +33,33 @@ public class UserDAO {
             Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
             Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }finally{
+        } finally {
             DBConnection.closeConnection(connection, ps);
         }
         return 0;
     }
-    
+
+    public static int createUser(PlayerBoard player) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        try {
+            connection = DBConnection.getConnection();
+            ps = connection.prepareStatement("INSERT INTO user(name) VALUES(?)");
+            ps.setString(1, player.getNickname());
+            ps.executeUpdate();
+            ps = connection.prepareStatement("SELECT MAX(id_user) FROM user");
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            int idUser = rs.getInt(1);
+            return idUser;
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            DBConnection.closeConnection(connection, ps);
+        }
+        return 0;
+    }
+
 }
