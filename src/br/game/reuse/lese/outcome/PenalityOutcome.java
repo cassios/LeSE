1c796d77a6d3311771b25a3a038002be5bbcba59
bruno <bruno@linux-PC>
2016-05-18 15:03:27 -0300

    Classes refactored.

--- src/br/game/reuse/lese/outcome/PenalityOutcome.java
+++ src/br/game/reuse/lese/outcome/PenalityOutcome.java
@@ -7,8 +7,6 @@ package br.game.reuse.lese.outcome;
 
 import br.game.reuse.lese.board.PlayerBoard;
 import br.game.reuse.lese.house.House;
-import br.game.reuse.lese.house.JokerHouse;
-import br.game.reuse.lese.question.JokerBoard;
 
 /**
  *
@@ -22,19 +20,8 @@ public class PenalityOutcome extends HouseOutcome {
 
     @Override
     public void apply(PlayerBoard p, House house) {
-        HouseOutcome outcome = house.getOutcome();
-        JokerHouse joker = (JokerHouse) house;
-        if (outcome instanceof BonusOutcome) {
-            System.out.println(joker.getJoker().getTitle() + "\n" + joker.getJoker().getDescription()
-                    + "\nVocê ganhou " + getPoints() + " pontos. Avance " + getNumberOfHouses() + " casa(s).");
-            p.creditPoints(getPoints());
-            p.move(getNumberOfHouses());
-        } else {
-            System.out.println(joker.getJoker().getTitle() + "\n" + joker.getJoker().getDescription()
-                    + "\nVocê perdeu " + getPoints() + " pontos. Recue " + getNumberOfHouses() + " casa(s).");
-            p.debitPoints(getPoints());
-            p.move(-getNumberOfHouses());
-        }
+        p.debitPoints(getPoints());
+        p.move(getNumberOfHouses());
     }
 
 }
