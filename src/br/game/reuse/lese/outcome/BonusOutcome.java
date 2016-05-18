1e5690dfb2f913cc81a985d6352bf2af41a7ac5c
bruno <bruno@linux-PC>
2016-05-18 15:03:27 -0300

    Classes refactored.

--- src/br/game/reuse/lese/outcome/BonusOutcome.java
+++ src/br/game/reuse/lese/outcome/BonusOutcome.java
@@ -9,8 +9,6 @@ import br.game.reuse.lese.board.PlayerBoard;
 import br.game.reuse.lese.house.House;
 import br.game.reuse.lese.house.IntermediateHouse;
 import br.game.reuse.lese.house.QuestionHouse;
-import br.game.reuse.lese.presenters.consolepresenters.ConsoleQuestionPresenter;
-import br.game.reuse.lese.presenters.interfaces.QuestionPresenter;
 
 /**
  *
@@ -30,25 +28,17 @@ public class BonusOutcome extends HouseOutcome {
     public void apply(PlayerBoard p, House house) {
         if (house instanceof QuestionHouse) {
             QuestionHouse question = (QuestionHouse) house;
-            QuestionPresenter questionPresenter = new ConsoleQuestionPresenter();
             if (question.isCorrect()) {
-                String feedback = "Parabéns, você acertou!!!\n" + question.getQuestion().getEplanation()
-                        + "\nVocê ganhou " + getPoints() + " pontos. Avance " + getNumberOfHouses() + " casa(s).";
-                questionPresenter.showFeedback(feedback);
                 p.creditPoints(getPoints());
                 p.move(getNumberOfHouses());
             } else {
-                String feedback = "Que pena, você errou.\n" + question.getQuestion().getEplanation()
-                        + "\nVocê perdeu " + getPoints() + " pontos.";
-                questionPresenter.showFeedback(feedback);
                 p.debitPoints(getPoints());
-//                p.move(-getNumberOfHouses());
+                p.move(-getNumberOfHouses());
             }
-        } else if (house instanceof IntermediateHouse) {
-            p.creditPoints(getPoints());
-            p.increasingCycle();
-            p.move(getNumberOfHouses());
         } else {
+            if (house instanceof IntermediateHouse) {
+                p.increasingCycle();
+            }
             p.creditPoints(getPoints());
             p.move(getNumberOfHouses());
         }
